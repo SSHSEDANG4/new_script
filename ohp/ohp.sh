fa9d453360eee5c9641e3f1e2ece8258a27152a6
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-12-07 08:45:40 +0700

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -4,13 +4,13 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/faiz007t/premium/main/iptext.sh | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/kunci_script/main/iptext.sh | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e "${green} Please Wait, Proses...${NC}"
 sleep 5
 else
-echo "User premium Only ,Sila Contact Admin Bot😘"
+echo "User premium Only ,Sila Contact Admin"
 exit 0
 fi
 clear
@@ -91,7 +91,7 @@ chmod +x /usr/local/bin/ohpserver
 cat > /etc/systemd/system/dropbear-ohp.service << END
 [Unit]
 Description=DROPBEAR OHP Redirection Service
-Documentation=https://t.me/onepiecevpn
+Documentation=https://t.me/sshsedang4
 After=network.target nss-lookup.target
 
 [Service]
@@ -133,7 +133,7 @@ END
 cat > /etc/systemd/system/dropbear-ohp.service << END
 [Unit]
 Description=DROPBEAR OHP Redirection Service
-Documentation=https://t.me/onepiecevpn
+Documentation=https://t.me/sshsedang4
 After=network.target nss-lookup.target
 
 [Service]
@@ -155,7 +155,7 @@ cat > /etc/systemd/system/ssh-ohp.service << END
 
 [Unit]
 Description=OpenVPN OHP Redirection Service
-Documentation=https://t.me/onepiecevpn
+Documentation=https://t.me/sshsedang4
 After=network.target nss-lookup.target
 
 [Service]
@@ -187,4 +187,4 @@ echo -e "Port Dropbear OHP: $Port_Dropbear"
 echo -e "Port OVPN OHP TCP: $Port_OHP"
 echo -e "Porr SSH OHP     : $Port_ssh"
 echo -e "Link Download OVPN OHP: http://$MYIP:81/tcp-ohp.ovpn"
-echo -e "Script By OnepieceVPN"
+echo -e "Script By SSH SEDANG NETWORK"
