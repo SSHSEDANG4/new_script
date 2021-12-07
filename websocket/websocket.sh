a5aee40644f997bfd84aad243e589433fbdf2750
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-12-07 08:47:05 +0700

    Update websocket.sh

--- websocket/websocket.sh
+++ websocket/websocket.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/faiz007t/premium/main/iptext.sh | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/kunci_script/main/iptext.sh | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e "${green} Please Wait, Proses...${NC}"
@@ -21,16 +21,16 @@ cd
 cd /etc/systemd/system/
 
 #Install system auto run
-wget -O /etc/systemd/system/ws-ssh.service https://raw.githubusercontent.com/faiz007t/bc/main/websocket/ws-ssh.service && chmod +x /etc/systemd/system/ws-ssh.service
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/faiz007t/bc/main/websocket/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/faiz007t/bc/main/websocket/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/faiz007t/bc/main/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-ssh.service https://raw.githubusercontent.com/SSHSEDANG4/new_script/main/websocket/ws-ssh.service && chmod +x /etc/systemd/system/ws-ssh.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/SSHSEDANG4/new_script/main/websocket/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/SSHSEDANG4/new_script/main/websocket/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/SSHSEDANG4/new_script/main/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 #Install Websocket-SSH
-wget -O /usr/local/bin/ws-ssh https://raw.githubusercontent.com/faiz007t/bc/main/websocket/ws-openssh.py && chmod +x /usr/local/bin/ws-ssh
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/faiz007t/bc/main/websocket/ws-dropbear.py && chmod +x /usr/local/bin/ws-dropbear
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/faiz007t/bc/main/websocket/ws-ovpn.py && chmod +x /usr/local/bin/ws-ovpn
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/faiz007t/bc/main/websocket/ws-tls.py && chmod +x /usr/local/bin/ws-stunnel
+wget -O /usr/local/bin/ws-ssh https://raw.githubusercontent.com/SSHSEDANG4/new_script/main/websocket/ws-openssh.py && chmod +x /usr/local/bin/ws-ssh
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/SSHSEDANG4/new_script/main/websocket/ws-dropbear.py && chmod +x /usr/local/bin/ws-dropbear
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/SSHSEDANG4/new_script/main/websocket/ws-ovpn.py && chmod +x /usr/local/bin/ws-ovpn
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/SSHSEDANG4/new_script/main/websocket/ws-tls.py && chmod +x /usr/local/bin/ws-stunnel
 
 #Enable, Start & Restart ws-ssh service
 systemctl enable ws-ssh.service
